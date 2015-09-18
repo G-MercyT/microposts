32f80d1a3406b79c607f20e7491ce9ae654d6108
g_mercyt <goripom4@gmail.com>
2015-09-18 15:47:31 +0000

    Gemfile update for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  before_action :set_locale
+
+  # 全リンクにlocale情報をセットする
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+  
+  # リンクの多言語化に対応する
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
   private
   def logged_in_user
     unless logged_in?
